2cc55722ea508c590b526e7ba7e03f0432123fb5
yunitacahya <yunitacahya998@gmail.com>
2025-07-04 11:37:29 +0800

    Add files via upload

---
+++ OOP-UAS-C-2301010337-main/uas-oop/src/uas/oop/UasOop.java
@@ -0,0 +1,17 @@
+package uas.oop;
+
+/**
+ *
+ * @author Yunita Cahya
+ * TGL 28 JUNI 2025
+ */
+public class UasOop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
